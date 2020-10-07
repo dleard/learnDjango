0e9d5b0246c3c3652247434d2377168c3d012b54
Dylan Leard <dylan@button.is>
2020-10-07 10:45:39 -0700

    step2: reconfigure db options for postgres & migrate

--- learndjango/learndjango/settings.py
+++ learndjango/learndjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'learndjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'HOST': 'localhost',
+        'USER': 'dleard',
+        'PASSWORD': '',
+        'PORT': '',
     }
 }
 
