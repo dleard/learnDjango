3dfe9e76ae3ccf083bec164699d8afda1b67c938
Dylan Leard <dylan@button.is>
2020-10-08 10:26:53 -0700

    add templates directory and apply to index view

--- learndjango/polls/views.py
+++ learndjango/polls/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+# The render() function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument. It returns an HttpResponse object of the given template rendered with the given context.
 
 from .models import Question
 
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  output = ', '.join([q.question_text for q in latest_question_list])
-  return HttpResponse(output)
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
+
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  template = loader.get_template('polls/index.html')
+  context = {
+    'latest_question_list': latest_question_list,
+  }
+  return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
   return HttpResponse("You're looking at question %s." % question_id)
