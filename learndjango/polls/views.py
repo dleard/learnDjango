07dfadd38aeeae92bc01aedda86da2c71b405ae6
Dylan Leard <dylan@button.is>
2020-10-08 10:34:38 -0700

    add shortcuts to views, apply template to detail

--- learndjango/polls/views.py
+++ learndjango/polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 # The render() function takes the request object as its first argument, a template name as its second argument and a dictionary as its optional third argument. It returns an HttpResponse object of the given template rendered with the given context.
+# The get_object_or_404() function takes a Django model as its first argument and an arbitrary number of keyword arguments, which it passes to the get() function of the model’s manager. It raises Http404 if the object doesn’t exist.
 
 from .models import Question
 
@@ -11,16 +13,9 @@ def index(request):
   context = {'latest_question_list': latest_question_list}
   return render(request, 'polls/index.html', context)
 
-def index(request):
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
-  context = {
-    'latest_question_list': latest_question_list,
-  }
-  return HttpResponse(template.render(context, request))
-
 def detail(request, question_id):
-  return HttpResponse("You're looking at question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
   response = "You're looking at the results of question %s"
